9a3577952d222ca84a8cf28ed41f75a7d6dd6d3a
Ediana-Ekanem <edianaekanem@gmail.com>
2023-12-29 13:44:34 +0100

    created container

---
+++ src/components/Container.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Container = ({ children }) => {
+  return (
+    <div className=" w-full md:max-w-5xl max-w-sm mx-autogit">{children}</div>
+  );
+};
+
+export default Container;
